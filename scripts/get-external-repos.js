bc9946733312f64f1f5ffa447770325e31453e03
Arthur Clemens <arthurclemens@gmail.com>
2017-03-05 00:54:04 +0100

    Add Zondicons

--- scripts/get-external-repos.js
+++ scripts/get-external-repos.js
@@ -1,18 +1,49 @@
 'use strict';
 
-var sh = require('shelljs');
+const sh = require('shelljs');
+const http = require('http');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
 
-var root = process.cwd();
-var dir = 'external-repos';
+const root = process.cwd();
+const dir = 'external-repos';
 sh.mkdir('-p', dir);
 
-function getMdi() {
-    console.log('Fetching Templarian Material Design Icons');
-    sh.cd(dir);
-    var repoDir = 'MaterialDesign';
-    sh.rm('-rf', repoDir);
-    sh.mkdir('-p', repoDir);
-    sh.exec('git clone https://github.com/Templarian/MaterialDesign.git');
+const getMdi = () => {
+  console.log('Fetching Templarian Material Design Icons');
+  sh.cd(root);
+  sh.cd(dir);
+  const repoDir = 'MaterialDesign';
+  sh.rm('-rf', repoDir);
+  sh.mkdir('-p', repoDir);
+  sh.exec('git clone https://github.com/Templarian/MaterialDesign.git');
+}
+
+const getZondicons = () => {
+  console.log('Fetching Zondicons');
+  sh.cd(root);
+  sh.cd(dir);
+  const repoDir = 'Zondicons';
+  sh.rm('-rf', repoDir);
+  sh.mkdir('-p', repoDir);
+  
+  const downloadZip = function(filename, url) {
+    const tmpFilePath = "/tmp/" + filename + ".zip"
+    http.get(url, function(response) {
+      response.on('data', function(data) {
+        fs.appendFileSync(tmpFilePath, data)
+      });
+      response.on('end', function() {
+        const zip = new AdmZip(tmpFilePath)
+        zip.extractAllTo(repoDir + "/" + filename)
+        fs.unlink(tmpFilePath, function() {
+          //
+        })
+      })
+    });
+  }
+  downloadZip("zondicons", "http://www.zondicons.com/zondicons.zip");
 }
 
 getMdi();
+getZondicons();
